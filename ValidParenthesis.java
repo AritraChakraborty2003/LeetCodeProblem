5e39cc9c8eb6434102bacb494e2ed6415397e558
Aritra314 <aritrachakraborty314@gmail.com>
2022-12-31 00:26:36 +0530

    initial commit

--- ValidParenthesis.java
+++ ValidParenthesis.java
@@ -5,20 +5,18 @@ import java.util.Stack;
 public class ValidParenthesis {
     public boolean isValid(String s) {
         //Stack Data Structure
-        Stack<Character> stk=new Stack<>();
-        for(int i=0;i<s.length();i++){
-            if(s.charAt(i)=='(' || s.charAt(i)=='{' || s.charAt(i)=='['){
+        Stack<Character> stk = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '[') {
                 stk.push(s.charAt(i));
-            }
-            else if(s.charAt(i)==')' || s.charAt(i)=='}' || s.charAt(i)==']'){
-                if(stk.size()==0)
+            } else if (s.charAt(i) == ')' || s.charAt(i) == '}' || s.charAt(i) == ']') {
+                if (stk.size() == 0)
                     return false;
-                else{
-                    char match=stk.peek();
-                    if((match=='(' && s.charAt(i)==')') || (match=='{' && s.charAt(i)=='}') || (match=='[' && s.charAt(i)==']')){
+                else {
+                    char match = stk.peek();
+                    if ((match == '(' && s.charAt(i) == ')') || (match == '{' && s.charAt(i) == '}') || (match == '[' && s.charAt(i) == ']')) {
                         stk.pop();
-                    }
-                    else{
+                    } else {
                         return false;
                     }
 
@@ -28,13 +26,13 @@ public class ValidParenthesis {
             }
 
 
-
         }
 
-        if(stk.size()!=0)
+        if (stk.size() != 0)
             return false;
 
         return true;
+    }
 
         public boolean isValid(String s) {
             //Stack Data Structure
